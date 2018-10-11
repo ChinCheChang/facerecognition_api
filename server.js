7e80572c7550b94d5547e493d6cb32557c19b708
ChinCheChang <36709019+ChinCheChang@users.noreply.github.com>
2018-10-11 17:17:53 +0800

    make code cleaner

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
-app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)});
-app.put('/image', (req, res) => {image.handleImage(req, res, db)});
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)});
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)});
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db)});
+app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 
 app.listen(3001, () => {
   console.log('app is running on port 3001');
