ed118410b10c6966c0b3711b66f2cf52d56b8549
ChinCheChang <36709019+ChinCheChang@users.noreply.github.com>
2018-10-12 14:24:32 +0800

    Move 'clarifai' API to the back end

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,17 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '46dc3702be414e7d80932121627f1e21'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json('unable to use with API'))
+}
+
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
 
@@ -11,5 +25,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-  handleImage: handleImage
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
 }
